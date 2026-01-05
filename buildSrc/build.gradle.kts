089289502823e11859467d2796f18b0bb95f24b6
faizan <faizanhmd3@gmail.com>
2026-01-05 17:07:47 +0500

    Project Setup

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+}
---
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -0,0 +1,13 @@
+object ProjectConfig {
+    const val compileSdk = 35
+    const val minSdk = 24
+    const val targetSdk = 35
+
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val applicationId = "app.phayzee.modernmulti_modulearchitectureandroid"
+
+    const val jvmTarget = "17"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
